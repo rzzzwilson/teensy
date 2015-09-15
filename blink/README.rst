ae9299a853b86d3913199c4faf3321387bb355c9
Ross Wilson <rzzzwilson@gmail.com>
2015-09-15 12:11:53 +0700

    Added 'no delay' double blink example

--- blink/README.rst
+++ blink/README.rst
@@ -5,14 +5,26 @@ The teensy `tutorials <https://www.pjrc.com/teensy/tutorial.html>`_ start
 with a 'blink' example which is a very simple *hello world!* example that
 blinks the on-board LED.
 
-**blink.c** is the original teensy program, one second on, on second off.
+**Blink.c** is the original teensy program, one second on, on second off.
 
 **blink_double.c** blinks the LED twice rapidly with a one second repeat time.
 Getting this to work exercises the Arduino build system.
 
+**BlinkWithoutDelay.c** is a Blink.c variation where the current time is polled
+in a loop allowing something to be done *besides* busy waiting.
+
+**blink_without_delay_double.c** is my rewrite of *BlinkWithoutDelay.c* to
+double blink the LED using the time poll approach.  This program uses a state
+machine to handle the more complex double blink.  I've also added a second state
+machine to handle printing something back to the serial monitor every 1.5
+seconds.
+
+Gripe
+-----
+
 The Arduino build system wants it's own filetypes - won't open and read a \*.c
-file!  I have to cut and paste the code into the IDE.  And I also have to cut
-and paste code _back_ into the source code file as the IDE won't let me save
-anywhere in the filesyste but only to a few selected places.  GRRRRR.
+file!  I have to cut and paste the code into the IDE!!  And I also have to cut
+and paste changed code _back_ into the source code file as the IDE won't let me
+save *anywhere* in the filesystem but only to a few selected places!!!  GRRRRR.
 
 Must switch to a command line environment as soon as possible.
---
+++ blink/blink_without_delay_double.c
@@ -0,0 +1,99 @@
+/*
+    Double blink LED and repeat every second.
+
+    Poll the time and do something if enough time has elapsed.  This allows the
+    code to do something else (a bit) while waiting for the time to elapse
+    without busy waiting.
+
+    The "something else" here is another state machine printing USB debug every
+    1.5 seconds.
+*/
+
+// constants defining the cycle time(s) (all in milliseconds)
+const int LedOn = 100;
+const int LedOff = 100;
+const int CycleWait = 1000 - 2*(LedOn + LedOff);
+
+const int UsbWait = 1500;
+
+// teensy 3.1 has the LED on pin 13
+const int ledPin = 13;
+
+void setup()
+{
+    // set the digital pin as output:
+    pinMode(ledPin, OUTPUT);      
+    // prepare the serial port
+    Serial.begin(38400);
+}
+
+void loop()
+{
+    // get current time
+    long currentMillis = (long) millis();
+
+    // pass current time to LED state machine
+    led_state_machine(currentMillis);
+
+    // and to the USB state machine
+    usb_state_machine(currentMillis);
+}
+
+// a simple state machine to write debug to the USB port every 1.5 seconds
+void usb_state_machine(long time)
+{
+    static long next_time = 0;
+    static int state = 0;
+
+    if (time > next_time)
+    {
+        switch (state)
+        {
+            case 0:                        // initialization
+                next_time = time;
+                /* FALL THROUGH */
+            case 1:                        // write debug
+                Serial.println("Hello World");
+                next_time += UsbWait;
+                state = 1;
+                break;
+        }
+    }
+}
+
+// a simple state machine to flash the LED
+void led_state_machine(long time)
+{
+    static long next_time = 0;
+    static int state = 0;
+
+    if (time > next_time)
+    {
+        switch (state)
+        {
+            case 0:                        // initialization
+                next_time = time;
+                /* FALL THROUGH */
+            case 1:                        // first flash, ON
+                digitalWrite(ledPin, HIGH);
+                next_time += LedOn;
+                state = 2;
+                break;
+            case 2:                        // first flash, OFF
+                digitalWrite(ledPin, LOW);
+                next_time += LedOff;
+                state = 3;
+                break;
+            case 3:                        // second flash, ON
+                digitalWrite(ledPin, HIGH);
+                next_time += LedOn;
+                state = 4;
+                break;
+            case 4:                        // second flash, OFF
+                digitalWrite(ledPin, LOW);
+                next_time += CycleWait;
+                state = 1;
+                break;
+        }
+    }
+}

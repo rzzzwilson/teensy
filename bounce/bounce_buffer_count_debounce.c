35961a3074f3aaf748a01bb8eee806ab1d2ba902
Ross Wilson <rzzzwilson@gmail.com>
2015-09-20 10:55:33 +0700

    Working on software debounce

--- bounce/bounce_buffer_count_debounce.c
+++ bounce/bounce_buffer_count_debounce.c
@@ -1,91 +1,86 @@
 /*
-    Code to count the number of "bounces" on switch open or close.
-    The data is buffered in-memory and dumped to the serial port
-    after the switch has settled.
+    Code to count the number of "bounces" on switch open or close.  The switch
+    is software debounced.  The data is buffered in-memory and dumped to the
+    serial port after the switch has settled.  We should see no bounces.
 
-    Derived from "bounce_buffer.c".
+    The switch debounce code is test only.  We will create a class to wrap
+    the debounce code.
+
+    Derived from "bounce_buffer_count.c".
 */
+
+#include <TimerOne.h>
+
+//////////////////////////////////////////////////////////////
+// values here are "switch" object variables
+// will go into a class later
+
+// Debounced switch state
+bool DebouncedSwitchState = false;
+
+// latest actual switch state
+bool LastState = false;
+
+// last state as found by timer routine
+bool LastTimerState = true;
+
+// debounce timer (microseconds)
+const long DebounceTime = 10000;
+
+// timer needs to see TimerTimes unchanged state
+const int TimerTimes = 5;
+int TimerCount = 0;
+
+//////////////////////////////////////////////////////////////
 
 // the pin the switch is attached to
 const int SwitchPin = 7;
 // store this number of results
 const int MaxBuffer = 2048;
-// after this number of samples, the switch has settled
+
 const long int BounceLimit = 100000;
 
-bool LastState = false;
 unsigned long Count = 0;
 int BounceCount = 0;
 unsigned long CountBuffer[MaxBuffer];
 bool BoolBuffer[MaxBuffer];
 unsigned int Index = 0;
 
-#define CHECK_MSEC        5        // Read hardware every 5 msec
-#define PRESS_MSEC        10       // Stable time before registering pressed
-#define RELEASE_MSEC      100      // Stable time before registering released
-
-// This function reads the key state from the hardware.
-bool RawKeyPressed(void)
+void myTimer(void)
 {
-    return digitalRead(SwitchPin);
-}
-
-// This holds the debounced state of the key.
-bool DebouncedKeyPress = false;
-
-// Service routine called every CHECK_MSEC to debounce both edges
-void DebounceSwitch1(bool *Key_changed, bool *Key_pressed)
-{
-    static unsigned char Count = RELEASE_MSEC / CHECK_MSEC;
-    bool RawState;
-
-    *Key_changed = false;
-    *Key_pressed = DebouncedKeyPress;
-
-    RawState = RawKeyPressed();
-    if (RawState == DebouncedKeyPress)
+    cli();
+    bool state = digitalRead(7);
+    if (state == LastTimerState)
     {
-        // Set the timer which will allow a change from the current state.
-        if (DebouncedKeyPress)
-            Count = RELEASE_MSEC / CHECK_MSEC;
-        else
-            Count = PRESS_MSEC / CHECK_MSEC;
+	if (++TimerCount > TimerTimes)
+	{
+	    // settled
+            DebouncedSwitchState = state;
+	    TimerCount = 0;
+	}
     }
     else
     {
-        // Key has changed - wait for new state to become stable.
-        if (--Count == 0)
-        {
-            // Timer expired - accept the change.
-            DebouncedKeyPress = RawState;
-            *Key_changed=true;
-            *Key_pressed = DebouncedKeyPress;
-
-            // And reset the timer.
-            if (DebouncedKeyPress)
-                Count = RELEASE_MSEC / CHECK_MSEC;
-            else
-                Count = PRESS_MSEC / CHECK_MSEC;
-        }
+	TimerCount = 0;
+        LastTimerState = state;
     }
+    sei();
 }
 
-// Service routine called by a timer interrupt
-bool DebounceSwitch2()
+void isrCheckSwitch(void)
 {
-    static unsigned int State = 0; // Current debounce status
-
-    State = (State<<1) | !RawKeyPressed() | 0xe000;
-    if (State==0xf000)
-	return true;
-    return false; 
+    cli();
+    LastState = digitalRead(7);
+    sei();
 }
 
 void setup()
 {
-    Serial.begin(38400);
+    Serial.begin(115200);
     pinMode(SwitchPin, INPUT);
-    LastState = not DebounceSwitch2();
+    attachInterrupt(SwitchPin, isrCheckSwitch, CHANGE);
+    Timer1.initialize(DebounceTime);
+    Timer1.attachInterrupt(myTimer);
 }
 
 void report(void)
@@ -97,9 +92,9 @@ void report(void)
 
         Serial.print(count);
         if (count == 1)
-            Serial.print(" change going to state ");
+            Serial.print(" edge going to state ");
         else
-            Serial.print(" changes going to state ");
+            Serial.print(" edges going to state ");
 
         if (state)
             Serial.println("OPEN");
@@ -112,15 +107,15 @@ void report(void)
 
 void loop()
 {
-    bool state = DebounceSwitch2();
+    static bool last_state = false;
 
-    if (state == LastState)
+    if (DebouncedSwitchState == last_state)
     {
         if (Count == BounceLimit)
         {
             // switch has settled, save bounce count
             CountBuffer[Index] = BounceCount;
-            BoolBuffer[Index] = state;
+            BoolBuffer[Index] = DebouncedSwitchState;
             ++Index;
             BounceCount = 0;
         }
@@ -128,7 +123,7 @@ void loop()
     else
     {
         ++BounceCount;
-        LastState = state;
+        last_state = DebouncedSwitchState;
         Count = 0;
     }
 

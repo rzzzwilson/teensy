f6fa1d9a6fb90f85d22799969ac890f621b625a7
Ross Wilson <rzzzwilson@gmail.com>
2015-09-22 19:40:39 +0700

    First analog output test program

---
+++ analog_output/analog.c
@@ -0,0 +1,56 @@
+/*
+    Analog output - place a symmetric sawtooth voltage onto pin A0.
+
+    Use delay() calls in loop() to do this.
+*/
+
+// the analog pin to write voltage to
+const int analogOutPin = A0;
+// LED to flash every sawtooth cycle start
+const int ledPin = LED_BUILTIN;        // pin 13 for teensy 3.1
+
+// states of sawtooth wave
+#define W_RISING        0
+#define W_FALLING        1
+bool waveState = W_RISING;
+
+void setup()
+{
+    Serial.begin(38400);
+    pinMode(ledPin, OUTPUT);
+}
+
+void blinkLED(void)
+{
+    digitalWrite(ledPin, HIGH);
+    delay(100);
+    digitalWrite(ledPin, LOW);
+}
+
+void loop()
+{
+    static int value = 0;
+
+    Serial.print("Writing value=");
+    Serial.println(value);
+    analogWrite(analogOutPin, value);
+
+    if (waveState == W_RISING)
+    {
+        if (++value >= 1024)
+        {
+            value = 1023;
+            waveState = W_FALLING;
+        }
+    }
+    else
+    {
+        if (--value <= 0)
+        {
+            value = 0;
+            waveState = W_RISING;
+            blinkLED();
+        }
+    }
+    delay(20);
+}
